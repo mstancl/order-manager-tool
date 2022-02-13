3914e614797e990369e39de0ae44308c44e2eadf
mstancl <mstancl94@gmail.com>
2022-02-13 19:25:08 +0100

    working edit

--- src/main/java/com/mstancl/ordermanagertool/controllers/mainScreen/MainScreenController.java
+++ src/main/java/com/mstancl/ordermanagertool/controllers/mainScreen/MainScreenController.java
@@ -96,7 +96,7 @@ public class MainScreenController {
 
     @FXML
     public void orderByID() {
-        List<Order> sortedList = orderDAO.getAllRecords();
+        List<Order> sortedList = listOfOrderFields.stream().map(OrderLineDetailFields::getOrder).collect(Collectors.toList());
 
         if (Comparators.isInOrder(sortedList, Comparator.comparing(Order::getId))) {
             sortedList.sort(Comparator.comparing(Order::getId).reversed());
@@ -110,7 +110,7 @@ public class MainScreenController {
 
     @FXML
     public void orderByDateWhenReceived() {
-        List<Order> sortedList = orderDAO.getAllRecords();
+        List<Order> sortedList = listOfOrderFields.stream().map(OrderLineDetailFields::getOrder).collect(Collectors.toList());
 
         if (Comparators.isInOrder(sortedList, Comparator.comparing(Order::getDateWhenReceived))) {
             sortedList.sort(Comparator.comparing(Order::getDateWhenReceived).reversed());
@@ -124,7 +124,7 @@ public class MainScreenController {
 
     @FXML
     public void orderByDueDate() {
-        List<Order> sortedList = orderDAO.getAllRecords();
+        List<Order> sortedList = listOfOrderFields.stream().map(OrderLineDetailFields::getOrder).collect(Collectors.toList());
 
         if (Comparators.isInOrder(sortedList, Comparator.comparing(Order::getDueDate))) {
             sortedList.sort(Comparator.comparing(Order::getDueDate).reversed());
@@ -139,7 +139,7 @@ public class MainScreenController {
 
     @FXML
     public void orderByEstimatedPrice() {
-        List<Order> sortedList = orderDAO.getAllRecords();
+        List<Order> sortedList = listOfOrderFields.stream().map(OrderLineDetailFields::getOrder).collect(Collectors.toList());
 
         if (Comparators.isInOrder(sortedList, Comparator.comparing(Order::getEstimatedPrice))) {
             sortedList.sort(Comparator.comparing(Order::getEstimatedPrice).reversed());
